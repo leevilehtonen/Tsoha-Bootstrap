5285930fdbd5d4041d710a4c98d4a228373fead8
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-07-25 21:38:51 +0300

    Etusivu suunnitelma luotu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,16 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+        View::make('helloworld.html');
+    }
+
+      public static function home()
+      {
+          View::make('suunnitelmat/home.html');
     }
   }
